edff763b57210db529d514765ff0443e420d553d
cgeek <cem.moreau@gmail.com>
2016-05-27 19:38:08 +0200

    Force list update

--- ui/app/js/controllers/MainController.js
+++ ui/app/js/controllers/MainController.js
@@ -4,16 +4,15 @@ module.exports = ($scope, $http, $state, WS) => {
 
   $scope.blocks = [];
 
-  let initialCall = true;
-
   WS.block((blocks) => {
     console.log(blocks);
     for (let i = 0, len = blocks.length; i < len; i++) {
       $scope.blocks.push(blocks[i]);
     }
-    if (!initialCall) {
+    try {
       $scope.$apply();
+    } catch (e) {
+      console.error(e);
     }
-    initialCall = false;
   });
 };
