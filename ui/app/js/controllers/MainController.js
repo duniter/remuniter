e6b309f78ce836fa0082c156a7ac2483ab6534a4
cgeek <cem.moreau@gmail.com>
2016-05-29 14:28:39 +0200

    Now displays last blocks, 24h and 7 days issuers

--- ui/app/js/controllers/MainController.js
+++ ui/app/js/controllers/MainController.js
@@ -4,13 +4,16 @@ const co = require('co');
 
 module.exports = ($scope, $http, $state, WS) => {
 
+  $scope.unit = 'units';
   $scope.blocks = [];
+  $scope.top1 = [];
+  $scope.top2 = [];
 
-  let socket = WS.block((blocks) => {
-    console.log(blocks);
-    for (let i = 0, len = blocks.length; i < len; i++) {
-      $scope.blocks.push(blocks[i]);
-    }
+  let socket = WS.block((data) => {
+    $scope.unit = data.unit;
+    $scope.blocks = data.blocks;
+    $scope.top1 = data.top1;
+    $scope.top2 = data.top2;
     try {
       $scope.$apply();
     } catch (e) {
