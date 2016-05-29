8d5eb6add471fcc52cc2f6181e7cf8a2d8939b01
cgeek <cem.moreau@gmail.com>
2016-05-29 17:54:18 +0200

    Add french + auto language selection

--- ui/app/js/lib/conf/translate.js
+++ ui/app/js/lib/conf/translate.js
@@ -2,10 +2,17 @@ module.exports = (app) => {
 
   app.config(['$translateProvider', ($translateProvider) => {
 
+    let lang = 'en';
+
     $translateProvider.translations('en', require('./i18n/en'));
+    $translateProvider.translations('fr', require('./i18n/fr'));
+
+    if (navigator.language && navigator.language.match(/^(en|fr)$/)) {
+      lang = navigator.language;
+    }
 
     // Default language
-    $translateProvider.preferredLanguage('en');
+    $translateProvider.preferredLanguage(lang);
 
     // Other parameters
     $translateProvider.useSanitizeValueStrategy('');
