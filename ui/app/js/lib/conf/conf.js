7d7102e3ff282393de91d5f053ca21be2d53a6bb
cgeek <cem.moreau@gmail.com>
2016-08-24 11:27:17 +0200

    Fix: default host must be "localhost"

--- ui/app/js/lib/conf/conf.js
+++ ui/app/js/lib/conf/conf.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "server": "l", // Empty server will use the browser current host
+  "server": "localhost", // Empty server will use the browser current host
   "port": "8555",   // Empty port will use the browser current port
   "default_port": 9220,
   "dev_autoconf": false,
