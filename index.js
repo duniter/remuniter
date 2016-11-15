1508a5916c43a23f017da28e31a701cb733c7c63
cgeek <cem.moreau@gmail.com>
2016-11-15 21:45:41 +0100

    Update to Duniter 0.50.1 API

--- index.js
+++ index.js
@@ -1,70 +1,47 @@
 #!/usr/bin/env node
 "use strict";
 
-const program = require('commander');
 const co = require('co');
-const spawn = require('child_process').spawn;
-const path = require('path');
-const pjson = require('./package.json');
+const duniter = require('duniter');
 const main = require('./lib/main.js');
-
-process.on('uncaughtException', function (err) {
-  console.error(err);
-  process.exit(1);
-});
 
 /****************************************
  * TECHNICAL CONFIGURATION
  ***************************************/
 
 const HOME_DUNITER_DATA_FOLDER = 'remuniter';
-const DUNITER_HTTP_LOGS = true;
 const SERVER_HOST = "localhost";
 const SERVER_PORT = 8555;
 
 /****************************************
- * PROGRAM COMMANDS
+ * STARTING DUNITER NODE
  ***************************************/
 
-program
-  .version(pjson.version)
-  .usage('<command> [options]');
-
-program
-  .command('start')
-  .description('Start the server.')
-  .action(() => main(SERVER_HOST, SERVER_PORT, HOME_DUNITER_DATA_FOLDER, DUNITER_HTTP_LOGS));
-
-
-/****************************************
- * SWITCH BETWEEN COMMANDS
- ***************************************/
-
-let duniter_subcommand = ['config', 'sync', 'reset', 'wizard'].indexOf(process.argv[2]) !== -1;
-if (duniter_subcommand) {
-
-  /**
-   * Forward command to Duniter
-   */
-  let duniterbin = path.resolve(path.dirname(process.argv[1]), './node_modules/duniter/bin/duniter');
-  let duniter_spawn = spawn(process.argv[0], [duniterbin].concat(process.argv.slice(2)).concat(['--mdb', HOME_DUNITER_DATA_FOLDER]));
-
-  process.stdin.pipe(duniter_spawn.stdin);
-  duniter_spawn.stdout.pipe(process.stdout);
-  duniter_spawn.stderr.pipe(process.stderr);
-
-  duniter_spawn.on('close', () => {
-    process.exit(0);
-  });
-} else {
-
-  /**
-   * Local program command
-   */
-  program.parse(process.argv);
-
-  if (program.args.length == 0) {
-    console.error('No command given');
-    process.exit(2);
-  }
+// Use netobs data folder
+if (!process.argv.includes('--mdb')) {
+  process.argv.push('--mdb');
+  process.argv.push(HOME_DUNITER_DATA_FOLDER);
 }
+
+// Default action = start
+if (process.argv.length === 4) process.argv.push('start');
+
+// Disable Duniter logs
+duniter.statics.logger.mute();
+
+duniter.statics.cli((duniterServer) => co(function*() {
+
+  try {
+
+    /****************************************
+     * SPECIALIZATION
+     ***************************************/
+
+    // Remuniter
+    main(duniterServer, SERVER_HOST, SERVER_PORT);
+
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}));
